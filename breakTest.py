b5760fbe5dfda0cad213c6ccc4be88a42b4af3f5
m-shahroodi <mojtaba.shahroodi@gmail.com>
2024-01-24 15:37:07 +0330

    my first 2 training python files

---
+++ breakTest.py
@@ -0,0 +1,12 @@
+# while True:
+#     a=int(input("Enter a : "))
+#     if a<0:
+#         break
+#     print(a)
+    
+for i in range(1,10):
+    for j in range(1,21):
+        print(j,end=" ")
+    if i>5 :
+        break
+    print()
