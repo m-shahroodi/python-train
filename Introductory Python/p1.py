295cdf5ce8053a7d92b347a859e28bed1cfa63e7
m-shahroodi <mojtaba.shahroodi@gmail.com>
2024-01-28 14:34:54 +0330

    train for create Module

---
+++ Introductory Python/p1.py
@@ -0,0 +1,12 @@
+# import myModule
+# print(myModule.sum(10,20))
+
+# import myModule as m 
+# print(m.mul(10,20))
+
+from myModule import mul,div
+print(mul(20,10))
+print(div(20,10))
+
+import math
+math.s
---
+++ Introductory Python/p2.py
@@ -0,0 +1,4 @@
+import classModule
+p2=classModule.Person("Ali",32)
+print(classModule.p1)
+print(p2)
